a3ffe6961f94c4e9b72e3a3d4eaeed7164334996
houteqwe <hout3e123@gmail.com>
2024-03-12 21:21:14 +0500

    task 4.3.16

---
+++ SfModule4/massive/Task 4.3.16.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4316
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+            int count = 0;
+            foreach (int i in arr)
+            {
+                if (i > 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
