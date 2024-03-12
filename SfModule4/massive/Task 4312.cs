c9978d4e8f8ec8f92d3142521d7e92caa33ab5b1
houteqwe <hout3e123@gmail.com>
2024-03-12 20:51:21 +0500

    Task 4.3.13

---
+++ SfModule4/massive/Task 4312.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4312
+{
+    class Program
+    {
+
+    public static void Main(string[] args)
+    {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach (var item in arr)                    
+                {
+                Console.WriteLine(item);
+                }
+            }
+        }
+  }
---
+++ SfModule4/massive/Task 4313.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4313
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int summ = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                summ += arr[i];
+            }
+            Console.WriteLine(summ);
+
+        }
+    }
+}
