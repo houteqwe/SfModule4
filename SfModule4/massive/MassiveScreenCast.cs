9d5eeced666b6227c55ca887c718a53bbeda36e6
houteqwe <hout3e123@gmail.com>
2024-03-12 20:00:28 +0500

    Task 4.3.7

---
+++ SfModule4/massive/MassiveScreenCast.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace MassiveScreenCast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 6, 30, 7, 14, 2, 15 };
+            int minValue = 100000,
+            minIndex = -1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < minValue)
+                {
+                    minValue = numbers[i];
+                    minIndex = i;
+                }
+            }
+
+            Console.WriteLine("Наименьший элемент массива имеет индекс {0}, значение этого элемента = {1}", minIndex, numbers[minIndex]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SfModule4/massive/Task437.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя");
+            var name = Console.ReadLine();
+            var namereverse = "";
+            for (int i = name.Length - 1; i >= 0; i--) {
+                namereverse += name[i];
+            }
+            Console.WriteLine("Ваше имя в обратном порядке: {0}", namereverse);
+            Console.ReadKey();
+        }
+    }
+}
