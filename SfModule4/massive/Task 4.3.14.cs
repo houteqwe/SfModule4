572521606706187926f12741b6f6a2708755f6e0
houteqwe <hout3e123@gmail.com>
2024-03-12 21:00:56 +0500

    Task 4.3.14

---
+++ SfModule4/massive/Task 4.3.14.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4314
+{
+    class Program
+{
+    public static void Main(string[] args)
+    {
+        int[][] array = new int[3][];
+        array[0] = new int[2] { 1, 2 };
+        array[1] = new int[3] { 1, 2, 3 };
+        array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
+        foreach (var num in array)
+        {
+            foreach (var item in num)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
+}
