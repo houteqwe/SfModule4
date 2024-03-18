f097851a66d302478c7d87cba81e8f32e792424d
houteqwe <hout3e123@gmail.com>
2024-03-18 16:48:10 +0500

    task 4.5.1

---
+++ SfModule4/Final Task Module 4/FinalTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTaskModule4
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            (string fName, string lName, string Login, int LoginLength, bool Pet, double Age, string favColors) User;
+
+
+    }
+}
