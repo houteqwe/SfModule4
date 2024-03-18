9cce38f2a2d2aa84fe3e5fdfcc42eef9cd92fdb5
houteqwe <hout3e123@gmail.com>
2024-03-18 16:37:46 +0500

    task 4.4.5

---
+++ SfModule4/cortege/Task 4.4.5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task445
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            (string Name, string Type, double Age, int NameCount) Pet;
+            
+            Console.Write("Введите имя питомца: ");
+            Pet.Name = Console.ReadLine();
+            Pet.NameCount = Pet.Name.Length;
+
+            Console.Write("Введите вид питомца: ");
+            Pet.Type = Console.ReadLine();
+
+            Console.Write("Введите возраст питомца: ");
+            Pet.Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+
+        }
+    }
+}
