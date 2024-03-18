9a82a722cadecce27d1a5a26672ca20900efaca3
houteqwe <hout3e123@gmail.com>
2024-03-18 16:24:05 +0500

    task 4.4.3

---
+++ SfModule4/cortege/Task 4.4.3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task443
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            var (name, age) = ("Игорь", 18);
+
+            Console.WriteLine("Мое имя: {0}", name);
+            Console.WriteLine("Мой возраст: {0}", age);
+
+            (string name, int age) anketa;
+            
+            Console.Write("Введите имя: ");
+            anketa.name = Console.ReadLine();
+            Console.Write("Введите возраст с цифрами: ");
+            anketa.age = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("Ваше имя: {0}", anketa.name);
+            Console.WriteLine("Ваш возраст: {0}", anketa.age);
+
+            Console.ReadKey();
+
+        }
+    }
+}
